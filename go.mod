df95d129760336ddf56c79f2a1638d2c48ecad53
satjan <82696795+satjan@users.noreply.github.com>
2023-06-02 21:58:47 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/satjan/go-logger
+
+go 1.18
+
+require github.com/sirupsen/logrus v1.9.2
+
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+
+require golang.org/x/sys v0.6.0 // indirect
---
+++ logrus.go
@@ -0,0 +1,27 @@
+package go_logrus
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func Logger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetReportCaller(true)
+	logger.SetFormatter(&logrus.JSONFormatter{
+		DataKey: "data",
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "@timestamp",
+			logrus.FieldKeyLevel: "log.level",
+			logrus.FieldKeyMsg:   "message",
+		},
+	})
+	file, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		logger.Out = file
+	} else {
+		logger.Info("Failed to log to file, using dsefault stderr")
+	}
+
+	return logger
+}

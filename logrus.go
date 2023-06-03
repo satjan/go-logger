8a2ad7ee609dbab3af46ac4c4800a99ecc791771
satjan <82696795+satjan@users.noreply.github.com>
2023-06-03 22:50:42 +0800

    Update logrus.go

--- logrus.go
+++ logrus.go
@@ -1,27 +1,56 @@
-package go_logrus
+package logger
 
 import (
 	"github.com/sirupsen/logrus"
-	"os"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"regexp"
 )
 
 var Log = logrus.New()
 
-func init() {
-	logger := logrus.New()
-	logger.SetReportCaller(true)
-	logger.SetFormatter(&logrus.JSONFormatter{
-		DataKey: "data",
-		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyTime:  "@timestamp",
-			logrus.FieldKeyLevel: "log.level",
-			logrus.FieldKeyMsg:   "message",
-		},
-	})
-	file, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err == nil {
-		logger.Out = file
-	} else {
-		logger.Info("Failed to log to file, using dsefault stderr")
+// CustomFormatter is a Logrus formatter that masks sensitive fields
+type CustomFormatter struct {
+	FieldNames []string
+	logrus.JSONFormatter
+}
+
+// Format formats the log entry and masks sensitive fields
+func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	for _, fieldName := range f.FieldNames {
+		re := regexp.MustCompile(fieldName)
+		entry.Message = re.ReplaceAllString(entry.Message, `********:"********"`)
 	}
+	return f.JSONFormatter.Format(entry)
+}
+
+func Init(filename string) {
+	Log.SetReportCaller(true)
+
+	// Create a custom formatter with sensitive fields to mask
+	formatter := &CustomFormatter{
+		FieldNames: []string{
+			`password:"[^"]+"`,
+			`passwordMatch:"[^"]+"`,
+			`token:"[^"]+"`,
+			`secret:"[^"]+"`,
+		},
+		JSONFormatter: logrus.JSONFormatter{
+			DataKey: "data",
+			FieldMap: logrus.FieldMap{
+				logrus.FieldKeyTime:  "@timestamp",
+				logrus.FieldKeyLevel: "log.level",
+				logrus.FieldKeyMsg:   "message",
+			},
+		},
+	}
+
+	Log.SetFormatter(formatter)
+
+	Log.SetOutput(&lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    5,    // Max size in megabytes before log rotation occurs
+		MaxBackups: 5,    // Max number of old log files to keep
+		MaxAge:     30,   // Max number of days to retain old log files
+		Compress:   true, // Whether to compress the rotated log files
+	})
 }
